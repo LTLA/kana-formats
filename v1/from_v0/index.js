75bc895f9140d9dff4991ac26868a90b9173dc1b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-03-09 20:59:57 -0800

    Generate the full exported state in the converter script.

--- v1/from_v0/index.js
+++ v1/from_v0/index.js
@@ -23,9 +23,26 @@ function bufferToNumber(buffer) {
     return output;
 }
 
+function numberToBuffer(number) {
+    // Store as little-endian. Probably safer
+    // than trying to cast it from a Uint64Array;
+    // not sure that endianness is strictly defined.
+    var output = new Uint8Array(8);
+
+    var i = 0;
+    while (number > 0) {
+        output[i] = number % 256;
+        number = Math.floor(number / 256);
+        i++;
+    }
+
+    return output;
+}
+
+/*** Reading the current state ***/
+
 let args = process.argv;
 let path = args[args.length - 1];
-console.log(path);
 const data = fs.readFileSync(path);
 let buffer = data.buffer;
 
@@ -39,8 +56,47 @@ offset += 8;
 var json_len = bufferToNumber(new Uint8Array(buffer, offset, 8));
 offset += 8;
 
+/*** Unpacking and converting ***/
+
 var contents = pako.ungzip(new Uint8Array(buffer, offset, json_len), { "to": "string" });
 let state = JSON.parse(contents);
-convertFromVersion0(state, "updated.h5");
+
+const newpath = "updated.h5";
+convertFromVersion0(state, newpath);
+
+/*** Reassembling the state ***/
+
+const newstate = fs.readFileSync(newpath);
+let newbuffer = new Uint8Array(newstate.buffer);
+
+{
+    let full = buffer.byteLength;
+    let start = 24 + json_len;
+    let remaining = new Uint8Array(buffer, start, full - start);
+
+    var combined = new ArrayBuffer(24 + newbuffer.length + remaining.length);
+    var combined_arr = new Uint8Array(combined);
+    let offset = 0;
+
+    let format_buf = numberToBuffer(format);
+    combined_arr.set(format_buf, offset); 
+    offset += format_buf.length;
+
+    let version = numberToBuffer(1000000);
+    combined_arr.set(version, offset); 
+    offset += version.length;
+
+    let newlen = numberToBuffer(newbuffer.length);
+    combined_arr.set(newlen, offset); 
+    offset += newlen.length;
+
+    combined_arr.set(newbuffer, offset);
+    offset += newbuffer.length;
+
+    combined_arr.set(remaining, offset);
+    offset += remaining.length;
+
+    fs.writeFileSync("full.h5", combined_arr);
+}
 
 scran.terminate();
